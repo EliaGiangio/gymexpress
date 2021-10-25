166b16dc802257a3721a5b68192677ef566f977c
EliaGiangio <eliagiang@yahoo.com>
2021-10-25 19:59:39 +0200

    Added gym show page and 1st database entry

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,4 +2,9 @@ class GymsController < ApplicationController
   def index
     @gyms = Gym.all
   end
+
+  def show
+   @gym = Gym.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "gyms#index"
 get "/gyms", to: "gyms#index"
+get "/gyms/:id", to:"gyms#show"
 end
