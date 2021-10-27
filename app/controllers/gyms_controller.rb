6b83cca84ce071117cbd160484b71ee9f3cf8fb1
EliaGiangio <eliagiang@yahoo.com>
2021-10-27 21:57:59 +0200

    Revert "Update/Delete/Partials"
    This reverts commit a447d55fbc758e4711ff4591992fe688e73028e6.

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -21,27 +21,6 @@ class GymsController < ApplicationController
     end
   end
 
-  def edit
-    @gym = Gym.find(params[:id])
-  end
-
-  def update
-    @gym = Gym.find(params[:id])
-
-    if @gym.update(gym_params)
-      redirect_to @gym
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @gym = Gym.find(params[:id])
-    @gym.destroy
-
-    redirect_to root_path
-  end
-
   private
     def gym_params
       params.require(:gym).permit(:name, :description)
